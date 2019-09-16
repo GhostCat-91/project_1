63a424b481e9a44c7cb0be4afb394f07ca8e2cc0
Praveen <praveen560857@gmail.com>
2019-09-16 14:35:49 +0000

    secnd edit for big3

--- big3.c
+++ big3.c
@@ -1,18 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 void big3()
 {
-   int num1,num2,num3;
-   
-   //Ask user to input any three integer numbers
-   printf("\n Enter Three numbers to check the biggest number among them:");
-   //Store input values in variables for comparsion
-   scanf("%d %d %d",&num1,&num2,&num3);
-
-   if((num1>num2)&&(num1>num3))
-      printf("\n num1 is greatest");
-   else if((num2>num3)&&(num2>num1))
-      printf("\n num2 is greatest");
-   else
-      printf("\n num3 is greatest");
-   //return 0;
+    double n1, n2, n3;
+    printf("Enter three numbers: ");
+    scanf("%lf %lf %lf", &n1, &n2, &n3);
+    if( n1>=n2 && n1>=n3)
+        printf("%.2lf is the largest number.", n1);
+    else if (n2>=n1 && n2>=n3)
+        printf("%.2lf is the largest number.", n2);
+    else
+        printf("%.2lf is the largest number.", n3);
+    //return 0;
 }
